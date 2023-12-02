d387b2387065c5ea96be2b9be4e7ea6ee2fe58d8
AgataZawadzka <agacia.zawadzka@gmail.com>
2023-12-02 22:17:53 +0100

    Wstępne ostylowanie elementów

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import './App.css';
+import './App.scss';
+import logo from './logo.svg';
 
 function App() {
   return (
-    <div className="App">
-        <h1>
-          Edit <code>src/App.js</code> and save to reload.
-        </h1 >
+    <div className="container">
+      <nav className='header'>
+      <img src={logo} alt="Logo" width='300px'/>
+      </nav>
+      <nav className="board"></nav>
+      <div class="columns">
+        <div className='column'>
+          <header>Nazwa kolumny</header>
+          <ul>
+            <li>Zadanie 1</li>
+            <li>Zadanie 2</li>
+          </ul>
+          <footer>Dodaj kolejne zadanie</footer>
+        </div>
+        <div className='column'>
+          <header>Nazwa kolumny</header>
+          <ul>
+            <li>Zadanie 1</li>
+            <li>Zadanie 2</li>
+            <li>Zadanie 1</li>
+            <li>Zadanie 2</li>
+            <li>Zadanie 1</li>
+            <li>Zadanie 2</li>
+          </ul>
+          <footer>Dodaj kolejne zadanie</footer>
+        </div>
+
+      </div>
     </div>
   );
 }
