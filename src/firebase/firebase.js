e0acf6bd26a87d7673ccfaefcacdd8671ff5dd97
AgataZawadzka <agacia.zawadzka@gmail.com>
2023-12-09 22:21:30 +0100

    Podpięcie firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCFflBFMH8GLRSLJ2iqvOW_Bkzh0ju54UU",
+  authDomain: "todotable-c5740.firebaseapp.com",
+  projectId: "todotable-c5740",
+  storageBucket: "todotable-c5740.appspot.com",
+  messagingSenderId: "1075637259347",
+  appId: "1:1075637259347:web:995d06ea369cc04d60caa6"
+};
+
+const app = initializeApp(firebaseConfig);
